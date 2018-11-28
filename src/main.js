5d43e5cd9ff72233a2fb2e5ea4fc469d87ed9c9b
myuon <ioi.joi.koi.loi@gmail.com>
2018-11-29 00:04:54 +0900

    feat: start from vuetify and firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import router from './router'
+import firebase from 'firebase'
+import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify);
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAL-NuKxMhShZVARoxzNMvXrGN3A65OEps",
+  authDomain: "portals-me.firebaseapp.com",
+  databaseURL: "https://portals-me.firebaseio.com",
+  projectId: "portals-me",
+  storageBucket: "portals-me.appspot.com",
+  messagingSenderId: "670077302427"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
