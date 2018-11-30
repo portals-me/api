5d1aa20c7e06454878ac81cfe2d885d78d14fe17
myuon <ioi.joi.koi.loi@gmail.com>
2018-11-30 21:04:43 +0900

    refactor: remove pact

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,23 +1,4 @@
-const { Pact } = require('@pact-foundation/pact');
-const path = require('path');
 const collections = require('@/sdk/collection');
-
-/*
-const provider = new Pact({
-  port: 8989,
-  log: path.resolve(__dirname, 'logs', 'example.log'),
-  dir: path.resolve(__dirname, 'pacts'),
-  spec: 2,
-  cors: true,
-  pactfileWriteMode: 'update',
-  consumer: 'Consumer',
-  provider: 'Provider',
-});
-
-beforeAll(() => provider.setup());
-afterEach(() => provider.verify());
-afterAll(() => provider.finalize());
-*/
 
 describe('The API', () => {
   describe('Collections', () => {
