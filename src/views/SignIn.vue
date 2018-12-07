039f48222a0515f968043deefdacbfd59672e282
myuon <ioi.joi.koi.loi@gmail.com>
2018-12-08 02:42:24 +0900

    feat: login ui with firebaseui

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -5,15 +5,18 @@
       wrap
     >
       <v-flex mb-5 xs12>
-        <v-btn @click="signInGoogle">
-          Sign in with Google
-        </v-btn>
+        <div id="firebaseui-auth-container" />
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css'
+
 export default {
   name: 'signin',
   methods: {
@@ -27,6 +30,20 @@ export default {
         console.log(token, user);
       });
     },
+  },
+  mounted () {
+    let ui = firebaseui.auth.AuthUI.getInstance();
+    if (!ui) {
+      ui = new firebaseui.auth.AuthUI(firebase.auth());
+    }
+
+    ui.start('#firebaseui-auth-container', {
+      signInFlow: 'redirect',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+      signInSuccessUrl: '/',
+    });
   },
 }
 </script>
