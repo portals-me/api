2709042116f91cb41f0821f6314bcd6424b59b47
myuon <ioi.joi.koi.loi@gmail.com>
2018-12-15 14:12:30 +0900

    feat: signin API with role

--- cdk/index.js
+++ cdk/index.js
@@ -1,5 +1,8 @@
 const cdk = require('@aws-cdk/cdk');
 const s3 = require('@aws-cdk/aws-s3');
+const lambda = require('@aws-cdk/aws-lambda');
+const apigateway = require('@aws-cdk/aws-apigateway');
+const iam = require('@aws-cdk/aws-iam');
 
 const service = 'portals-me';
 const stage = 'dev';
@@ -8,9 +11,36 @@ class MainStack extends cdk.Stack {
   constructor(parent, props) {
     super(parent, `${service}-${stage}`, props);
 
-    new s3.Bucket(this, 'StaticFileStore', {
-      bucketName: `${service}-web`,
+    new s3.Bucket(this, 'Hosting', {
+      bucketName: `${service}`,
     });
+
+    const role = new iam.Role(this, 'cognitoRoleForLambda', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+    role.addToPolicy(
+      new iam.PolicyStatement()
+        .addAllResources()
+        .addActions([
+          'logs:CreateLogGroup',
+          'logs:CreateLogStream',
+          'logs:PutLogEvents',
+          'cognito-identity:*',
+        ])
+    );
+
+    const signInHandler = new lambda.Function(this, 'SignInHandler', {
+      runtime: lambda.Runtime.NodeJS810,
+      code: lambda.Code.directory('src/functions'),
+      handler: 'auth.signIn',
+      role: role,
+    });
+
+    const api = new apigateway.RestApi(this, 'RestApi', {
+      restApiName: `${service}-${stage}`,
+    });
+
+    api.root.addResource('signIn').addMethod('POST', new apigateway.LambdaIntegration(signInHandler));
   }
 }
 
