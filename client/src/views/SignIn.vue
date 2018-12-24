46c470f0c823d475fdf5870b9ab235fde03451ea
myuon <ioi.joi.koi.loi@gmail.com>
2018-12-24 19:21:46 +0900

    feat: error message when signIn failed

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -5,6 +5,7 @@
       wrap
     >
       <v-flex mb-5 xs12>
+        <p style="color: red;">{{ this.errorMessage }}</p>
         <v-btn @click="signIn">Sign In with Google</v-btn>
       </v-flex>
     </v-layout>
@@ -15,13 +16,25 @@
 import sdk from '@/app/sdk';
 
 export default {
+  data () {
+    return {
+      errorMessage: '',
+    };
+  },
   methods: {
     async signIn () {
       const user = await this.$gAuth.signIn();
-      const result = (await sdk.signIn(user.getAuthResponse().id_token)).data;
-      localStorage.setItem('id_token', result.id_token);
-      this.$store.commit('setUser', result.user);
-      this.$router.push('/');
+
+      try {
+        const result = (await sdk.signIn(user.getAuthResponse().id_token)).data;
+
+        localStorage.setItem('id_token', result.id_token);
+        this.$store.commit('setUser', result.user);
+        this.$router.push('/');
+      } catch (err) {
+        this.errorMessage = err.response.data;
+        return;
+      }
     },
   },
   async mounted () {
