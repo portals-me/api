fa365741c207ba917664d77146899ab7da0fc308
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-12 16:52:40 +0900

    design: createArticleForm

--- client/src/views/Collection.vue
+++ client/src/views/Collection.vue
@@ -11,10 +11,71 @@
           depressed
           outline
           color="indigo"
+          @click="createArticleDialog = true"
         >
           <v-icon left>add</v-icon>
           作品を登録
         </v-btn>
+
+        <v-dialog
+          v-model="createArticleDialog"
+          max-width="760px"
+        >
+          <v-card>
+            <v-card-title
+              class="headline grey lighten-2"
+              primary-title
+            >
+              作品を登録
+            </v-card-title>
+
+            <v-card-text>
+              <v-form
+                v-model="createArticleForm.valid"
+                ref="createArticleForm"
+                lazy-validation
+              >
+                <v-text-field
+                  v-model="createArticleForm.url"
+                  label="URLを入力"
+                  required
+                />
+
+                <v-text-field
+                  v-model="createArticleForm.title"
+                  label="タイトル"
+                  :rules="[v => !!v || '必須項目です']"
+                  required
+                />
+
+                <v-textarea
+                  v-model="createArticleForm.description"
+                  label="説明(任意)"
+                  auto-grow
+                  rows="1"
+                />
+
+                <v-checkbox
+                  v-model="createArticleForm.checkbox"
+                  :rules="[v => !!v || '作品を登録できるのは正当な権利者のみです']"
+                  label="私はこの作品の正当な権利者であり、他のいかなる権利の侵害もしていません"
+                  required
+                >
+                </v-checkbox>
+
+                <v-btn
+                  :disabled="!createArticleForm.valid"
+                  color="indigo"
+                  dark
+                  @click="submit"
+                >
+                  <v-icon left>send</v-icon>
+                  送信
+                </v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
       </v-layout>
     </v-flex>
 
@@ -279,6 +340,15 @@ export default {
   data () {
     return {
       articleDialog: false,
+      createArticleDialog: false,
+      createArticleDialogTab: null,
+      createArticleForm: {
+        valid: true,
+        url: '',
+        checkbox: false,
+        title: '',
+        description: '',
+      },
     };
   },
   components: {
@@ -301,7 +371,12 @@ export default {
   methods: {
     clickArticleCard () {
       this.articleDialog = true;
-    }
+    },
+    submit () {
+      if (this.$refs.createArticleForm.validate()) {
+        console.log(this.createArticleForm);
+      }
+    },
   }
 }
 </script>
