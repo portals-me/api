d6159c634e177024f47b299bd46ce017b3523c1e
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-19 10:33:08 +0900

    chore(#7): dynamodb table

---
+++ infrastructure/modules/dynamodb/entities.tf
@@ -0,0 +1,30 @@
+variable "service" {}
+
+resource "aws_dynamodb_table" "entities" {
+  name = "${var.service}-entities"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "id"
+  range_key = "sort"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "sort"
+    type = "S"
+  }
+
+  attribute {
+    name = "owned_by"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = "owner"
+    hash_key = "owned_by"
+    range_key = "id"
+    projection_type = "ALL"
+  }
+}
