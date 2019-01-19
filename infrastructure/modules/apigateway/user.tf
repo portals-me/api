54f0a479c08041ee3a369ac5631af8a3f730d499
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-20 01:48:35 +0900

    chore: api for /users/{userid}

--- infrastructure/modules/apigateway/user.tf
+++ infrastructure/modules/apigateway/user.tf
@@ -17,3 +17,25 @@ module "users" {
     }
   ]
 }
+
+module "users-user" {
+  source = "lambda_api_path"
+
+  rest_api_id = "${aws_api_gateway_rest_api.restapi.id}"
+  parent_id = "${module.users.id}"
+  path_part = "{userId}"
+  methods_count = 1
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.lambda_authorizer.id}"
+
+  request_parameters = {
+    "method.request.path.userId" = true
+  }
+
+  methods = [
+    {
+      http_method = "GET"
+      function_arn = "${var.user_arn}"
+    }
+  ]
+}
