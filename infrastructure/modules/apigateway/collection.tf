a280e594bf6de3a42e36513ef9e6fd8c4a6a81a7
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-20 11:56:59 +0900

    feat: add /collection/{collectionId} api

--- infrastructure/modules/apigateway/collection.tf
+++ infrastructure/modules/apigateway/collection.tf
@@ -25,3 +25,29 @@ module "collections-cors" {
   api_id          = "${aws_api_gateway_rest_api.restapi.id}"
   api_resource_id = "${module.collections.id}"
 }
+
+module "collections-collection" {
+  source = "lambda_api"
+
+  rest_api_id = "${aws_api_gateway_rest_api.restapi.id}"
+  parent_id = "${module.collections.id}"
+  path_part = "{collectionId}"
+  methods_count = 1
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.lambda_authorizer.id}"
+
+  methods = [
+    {
+      http_method = "GET"
+      function_arn = "${var.collection_arn}"
+    }
+  ]
+}
+
+module "collections-collection-cors" {
+  source = "github.com/squidfunk/terraform-aws-api-gateway-enable-cors"
+  version = "0.2.0"
+
+  api_id          = "${aws_api_gateway_rest_api.restapi.id}"
+  api_resource_id = "${module.collections-collection.id}"
+}
