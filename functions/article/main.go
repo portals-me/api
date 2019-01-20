36cf3278ad820678932dff4450047edd69cfc4df
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-20 23:17:40 +0900

    feat: createArticle, listArticles

---
+++ functions/article/main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbattribute"
+
+	"github.com/aws/aws-lambda-go/events"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Collection struct {
+	ID             string            `json:"id"`
+	CommentMembers []string          `json:"comment_members"`
+	CommentCount   int               `json:"comment_count"`
+	Media          []string          `json:"media"`
+	Cover          map[string]string `json:"cover"`
+	OwnedBy        string            `json:"owned_by"`
+	Title          string            `json:"title"`
+	CreatedAt      int64             `json:"created_at"`
+	Sort           string            `json:"sort"`
+	Description    string            `json:"description"`
+}
+
+/* Entity Examples
+
+{
+	type: share
+	format: oembed
+	url: https://~~~
+}
+
+{
+	type: document
+	format: markdown
+	url: https://~~~
+}
+*/
+type Article struct {
+	ID          string            `json:"id"`
+	Entity      map[string]string `json:"entity"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	OwnedBy     string            `json:"owned_by"`
+	CreatedAt   int64             `json:"created_at"`
+}
+
+// for DynamoDB
+type ArticleDTO struct {
+	ID          string            `json:"id"`
+	Sort        string            `json:"sort"`
+	Entity      map[string]string `json:"entity"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	OwnedBy     string            `json:"owned_by"`
+	CreatedAt   int64             `json:"created_at"`
+}
+
+func (article Article) ToDTO(collectionID string) ArticleDTO {
+	return ArticleDTO{
+		ID:          "collection##" + collectionID,
+		Sort:        "article##" + article.ID,
+		Entity:      article.Entity,
+		Title:       article.Title,
+		Description: article.Description,
+		OwnedBy:     article.OwnedBy,
+		CreatedAt:   article.CreatedAt,
+	}
+}
+
+func (dto ArticleDTO) FromDTO() Article {
+	return Article{
+		ID:          strings.Replace(dto.Sort, "article##", "", 1),
+		Entity:      dto.Entity,
+		Title:       dto.Title,
+		Description: dto.Description,
+		OwnedBy:     dto.OwnedBy,
+		CreatedAt:   dto.CreatedAt,
+	}
+}
+
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	user := event.RequestContext.Authorizer
+
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	Dynamo := dynamodb.New(cfg)
+	collectionID := event.PathParameters["collectionId"]
+
+	if event.HTTPMethod == "GET" {
+		if _, ok := event.PathParameters["articleId"]; ok {
+		} else {
+			result, err := Dynamo.QueryRequest(&dynamodb.QueryInput{
+				TableName:              aws.String(os.Getenv("EntityTable")),
+				KeyConditionExpression: aws.String("owned_by = :owned_by and begins_with(id, :id)"),
+				ExpressionAttributeValues: map[string]dynamodb.AttributeValue{
+					":id": {
+						S: aws.String("collection##" + collectionID),
+					},
+					":sort": {
+						S: aws.String("article##"),
+					},
+				},
+			}).Send()
+
+			if err != nil {
+				return events.APIGatewayProxyResponse{}, err
+			}
+
+			var articles []Article
+			var articleDTOs []ArticleDTO
+			dynamodbattribute.UnmarshalListOfMaps(result.Items, &articleDTOs)
+			for _, value := range articleDTOs {
+				articles = append(articles, value.FromDTO())
+			}
+
+			out, _ := json.Marshal(articles)
+
+			return events.APIGatewayProxyResponse{
+				Body:       string(out),
+				Headers:    map[string]string{"Access-Control-Allow-Origin": "*"},
+				StatusCode: 200,
+			}, nil
+		}
+	} else if event.HTTPMethod == "POST" {
+		var createInput map[string]interface{}
+		json.Unmarshal([]byte(event.Body), &createInput)
+
+		articleID := uuid.Must(uuid.NewV4()).String()
+
+		// care for Entity struct
+		entityMap := map[string]string{}
+		entity := createInput["entity"].(map[string]interface{})
+		for key, value := range entity {
+			entityMap[key] = value.(string)
+		}
+
+		collection, err := dynamodbattribute.MarshalMap(Article{
+			ID:          articleID,
+			Entity:      entityMap,
+			Title:       createInput["title"].(string),
+			Description: createInput["description"].(string),
+			CreatedAt:   time.Now().Unix(),
+			OwnedBy:     user["id"].(string),
+		}.ToDTO(collectionID))
+
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+
+		_, err = Dynamo.PutItemRequest(&dynamodb.PutItemInput{
+			TableName: aws.String(os.Getenv("EntityTable")),
+			Item:      collection,
+		}).Send()
+
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+
+		return events.APIGatewayProxyResponse{
+			Headers: map[string]string{
+				"Access-Control-Allow-Origin": "*",
+				"Location":                    "/collections/" + collectionID + "/articles/" + articleID,
+			},
+			StatusCode: 201,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("%+v", event.PathParameters),
+		Headers:    map[string]string{"Access-Control-Allow-Origin": "*"},
+		StatusCode: 400,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
--- infrastructure/modules/apigateway/collection.tf
+++ infrastructure/modules/apigateway/collection.tf
@@ -1,4 +1,5 @@
 variable "collection_arn" {}
+variable "article_arn" {}
 
 module "collections" {
   source = "lambda_api"
@@ -58,4 +59,34 @@ module "collections-collection-cors" {
 
   api_id          = "${aws_api_gateway_rest_api.restapi.id}"
   api_resource_id = "${module.collections-collection.id}"
+}
+
+module "collections-collection-articles" {
+  source = "lambda_api"
+
+  rest_api_id = "${aws_api_gateway_rest_api.restapi.id}"
+  parent_id = "${module.collections-collection.id}"
+  path_part = "articles"
+  methods_count = 2
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.lambda_authorizer.id}"
+
+  methods = [
+    {
+      http_method = "POST"
+      function_arn = "${var.article_arn}"
+    },
+    {
+      http_method = "GET"
+      function_arn = "${var.article_arn}"
+    },
+  ]
+}
+
+module "collections-collection-articles-cors" {
+  source = "github.com/squidfunk/terraform-aws-api-gateway-enable-cors"
+  version = "0.2.0"
+
+  api_id          = "${aws_api_gateway_rest_api.restapi.id}"
+  api_resource_id = "${module.collections-collection-articles.id}"
 }
