573dfb4b03376b722b12c1591ce59f6c4e1e7d85
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-26 17:45:29 +0900

    feat: sign in with twitter flow

--- env.js
+++ env.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const env = {
   "GClientId": "670077302427-0r21asrffhmuhkvfq10qa8kj86cslojn.apps.googleusercontent.com",
   "EntityTable": "portals-me-entities",
-  "IdentityPoolId": "ap-northeast-1:5221828e-b1d8-45e7-9361-b06057573aa9",
+  "IdentityPoolId": fs.readFileSync('./token/cognito.key', 'utf8'),
   "JwtPrivate": fs.readFileSync('./token/jwtES256.key', 'utf8'),
   "JwtPublic": fs.readFileSync('./token/jwtES256.key.pub', 'utf8'),
   "TwitterKey": fs.readFileSync('./token/twitter.key', 'utf8'),
