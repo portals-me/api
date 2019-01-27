624c1a65d949f1f70d4dd0de27b35a9d00bc943e
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-27 19:59:53 +0900

    refactor: signer package

---
+++ functions/authenticator/signer/lib.go
@@ -0,0 +1,43 @@
+package signer
+
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+
+	"github.com/gbrlsnchs/jwt"
+)
+
+type ISigner interface {
+	Sign([]byte) ([]byte, error)
+}
+
+type ES256Signer struct {
+	Key string
+}
+
+func (signer ES256Signer) Sign(payload []byte) ([]byte, error) {
+	block, _ := pem.Decode([]byte(signer.Key))
+	privateKey, err := x509.ParseECPrivateKey(block.Bytes)
+	es256 := jwt.NewES256(privateKey, &privateKey.PublicKey)
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	header, _ := json.Marshal(map[string]string{
+		"alg": "ES256",
+		"typ": "JWT",
+	})
+
+	headerEnc := base64.StdEncoding.EncodeToString(header)
+	payloadEnc := base64.StdEncoding.EncodeToString(payload)
+	signed, err := es256.Sign([]byte(headerEnc + "." + payloadEnc))
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return signed, err
+}
