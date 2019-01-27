b17c8a7af2d8f40a6f5720516ca1bbd750d0bf35
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-27 14:35:04 +0900

    fix: load localStorage lazily

--- client/src/app/sdk.js
+++ client/src/app/sdk.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import api from '../../../src/api';
 const url = 'https://ibsrd4lyxk.execute-api.ap-northeast-1.amazonaws.com/dev';
-const token = localStorage.getItem('id_token');
-
-const sdk = api.genSDK(url, token, axios);
+const sdk = api.genSDK(url, () => localStorage.getItem('id_token'), axios);
 
 export default sdk;
