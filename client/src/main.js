7920346e2a076f4a5c311ff3df3407c69d1d1cbb
myuon <ioi.joi.koi.loi@gmail.com>
2019-01-30 00:07:10 +0900

    refactor: remove unused packages

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,7 @@ import 'vuetify/dist/vuetify.min.css'
 import router from './router'
 import vueConfig from 'vue-config'
 import App from './App.vue'
-import axios from 'axios'
 import store from '@/store'
-import GAuth from 'vue-google-oauth2'
-import VueAxios from 'vue-axios'
-import VueAuthenticate from 'vue-authenticate'
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -16,17 +12,6 @@ Vue.use(Vuetify);
 Vue.use(vueConfig, {
   API: 'https://v6bnqbi2hf.execute-api.ap-northeast-1.amazonaws.com/prod',
   isDev,
-  providers: require('../../token/auth.json'),
-});
-
-Vue.use(GAuth, {
-  clientId: '670077302427-0r21asrffhmuhkvfq10qa8kj86cslojn.apps.googleusercontent.com',
-  scope: 'profile email https://www.googleapis.com/auth/plus.login'
-});
-
-Vue.use(VueAxios, axios);
-Vue.use(VueAuthenticate, {
-  baseUrl: 'http://localhost:8080',
   providers: require('../../token/auth.json'),
 });
 
