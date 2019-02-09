7f3f04c784147ee2f1e8eb987e6bc0a6a55a2ee2
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-09 19:23:35 +0900

    feat(wip): dynamo says `ValidationException: The provided key element does not match the schema` but why?

--- infrastructure/modules/dynamodb/feeds.tf
+++ infrastructure/modules/dynamodb/feeds.tf
@@ -1,16 +1,28 @@
 resource "aws_dynamodb_table" "feeds" {
   name = "${var.service}-${var.stage}-feeds"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
-  range_key = "sort"
+  hash_key = "user_id"
+  range_key = "timestamp"
 
   attribute {
-    name = "id"
+    name = "user_id"
     type = "S"
   }
 
   attribute {
-    name = "sort"
+    name = "timestamp"
     type = "S"
+  }
+
+  attribute {
+    name = "item_id"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = "ItemID"
+    hash_key = "user_id"
+    range_key = "item_id"
+    projection_type = "KEYS_ONLY"
   }
 }
