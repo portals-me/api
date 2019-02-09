0795a8ea2e25d83814cc471db27e6093c730386b
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-09 19:23:35 +0900

    feat(wip): dynamo says `ValidationException: The provided key element does not match the schema` but why?

--- env.js
+++ env.js
@@ -4,6 +4,7 @@ const env = {
   "GClientId": "670077302427-0r21asrffhmuhkvfq10qa8kj86cslojn.apps.googleusercontent.com",
   "EntityTable": `portals-me-${process.argv[2]}-entities`,
   "SortIndex": `DataTable`,
+  "FeedTable": `portals-me-${process.argv[2]}-feeds`,
   "IdentityPoolId": fs.readFileSync('./token/cognito.key', 'utf8'),
   "JwtPrivate": fs.readFileSync('./token/jwtES256.key', 'utf8'),
   "JwtPublic": fs.readFileSync('./token/jwtES256.key.pub', 'utf8'),
--- functions/entity-stream/main.go
+++ functions/entity-stream/main.go
@@ -3,15 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
-
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/guregu/dynamo"
 )
 
+var db = dynamo.New(session.New(), &aws.Config{})
+var table = db.Table(os.Getenv("FeedTable"))
+
+type FeedEvent struct {
+	UserID    string `dynamo:"user_id"`
+	Timestamp int64  `dynamo:"timestamp"`
+	EventName string `dynamo:"event_name"`
+	ItemID    string `dynamo:"item_id"`
+}
+
 func handler(ctx context.Context, event events.DynamoDBEvent) error {
+	items := []interface{}{}
+
 	for _, record := range event.Records {
 		fmt.Printf("%+v\n", record)
+
+		if record.EventName == "INSERT" && strings.HasPrefix(record.Change.Keys["id"].String(), "collection##") {
+			feed := FeedEvent{
+				UserID:    record.Change.NewImage["sort_value"].String(),
+				Timestamp: record.Change.ApproximateCreationDateTime.Unix(),
+				EventName: "INSERT_COLLECTION",
+				ItemID:    record.Change.Keys["id"].String(),
+			}
+			items = append(items, feed)
+
+			s, _ := dynamo.MarshalItem(feed)
+			fmt.Println(s)
+		}
+	}
+
+	_, err := table.Batch().Write().Put(items...).Run()
+	if err != nil {
+		return err
 	}
 
 	return nil
