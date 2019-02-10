dd5a502f66f4e24a2ed7cafbb1799d2750d55435
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-10 15:41:22 +0900

    feat: confirm dialog for deleteCollection

--- client/src/views/Collection.vue
+++ client/src/views/Collection.vue
@@ -333,13 +333,49 @@
 
       <v-tab-item>
         <v-container>
-          <v-btn
-            color="error"
-            @click="deleteCollection"
+          <v-dialog
+            v-model="deleteDialog"
+            width="500"
           >
-            プロジェクトを削除
-            <v-icon right dark>delete</v-icon>
-          </v-btn>
+            <v-btn
+              color="error"
+              slot="activator"
+            >
+              プロジェクトを削除
+              <v-icon right dark>delete</v-icon>
+            </v-btn>
+
+            <v-card>
+              <v-card-title
+                class="headline grey lighten-2"
+                primary-title
+              >
+                削除しますか？
+              </v-card-title>
+
+              <v-card-text>
+                このコレクションを削除しますか？コレクションに属するアイテムもすべて削除され、この操作は取り消すことができません。
+              </v-card-text>
+
+              <v-divider />
+
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  color="error"
+                  @click="deleteCollection"
+                >
+                  削除
+                </v-btn>
+                <v-btn
+                  flat
+                  @click="deleteDialog = false"
+                >
+                  キャンセル
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-container>
       </v-tab-item>
     </v-tabs>
@@ -367,6 +403,7 @@ export default {
         description: '',
       },
       collection: {},
+      deleteDialog: null,
       articles: [],
       activeArticle: {},
       imageData: [],
