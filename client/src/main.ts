d5777f29bf12916d3d0792bab50bcb4350b3a22b
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-11 17:14:02 +0900

    chore: update for deploy

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import router from './router'
-import vueConfig from 'vue-config'
 import App from './App.vue'
 import store from '@/store'
+const vueConfig = require('vue-config');
 
 const isDev = process.env.NODE_ENV === 'development';
 
