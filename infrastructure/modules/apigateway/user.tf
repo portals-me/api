40798b61927ce7618e5083b7be58689dba17c28e
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-15 22:50:34 +0900

    chore: /user/userid/follow api

--- infrastructure/modules/apigateway/user.tf
+++ infrastructure/modules/apigateway/user.tf
@@ -77,3 +77,33 @@ module "users-user-feed-cors" {
   api_id          = "${aws_api_gateway_rest_api.restapi.id}"
   api_resource_id = "${module.users-user-feed.id}"
 }
+
+module "users-user-follow" {
+  source = "lambda_api_path"
+
+  rest_api_id = "${aws_api_gateway_rest_api.restapi.id}"
+  parent_id = "${module.users-user.id}"
+  path_part = "follow"
+  methods_count = 1
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.lambda_authorizer.id}"
+
+  request_parameters = {
+    "method.request.path.userId" = true
+  }
+
+  methods = [
+    {
+      http_method = "POST"
+      function_arn = "${var.user_arn}"
+    },
+  ]
+}
+
+module "users-user-follow-cors" {
+  source = "github.com/squidfunk/terraform-aws-api-gateway-enable-cors"
+  version = "0.2.0"
+
+  api_id          = "${aws_api_gateway_rest_api.restapi.id}"
+  api_resource_id = "${module.users-user-follow.id}"
+}
