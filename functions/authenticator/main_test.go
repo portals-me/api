73ff32b8c2a1b20fd0a70da49dd66bb717b42997
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-20 02:14:55 +0900

    fix: tests with dynamodb local

--- functions/authenticator/main_test.go
+++ functions/authenticator/main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/cognitoidentity/cognitoidentityiface"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbiface"
@@ -16,10 +19,15 @@ import (
 
 type fakeCustomProvider struct {
 	cognitoidentityiface.CognitoIdentityAPI
+	customID string
 }
 
 func (provider *fakeCustomProvider) GetIdpID(logins Logins) (string, error) {
-	return "fake-idp", nil
+	if provider.customID != "" {
+		return provider.customID, nil
+	} else {
+		return "fake-idp", nil
+	}
 }
 
 type operation struct {
@@ -74,10 +82,24 @@ func (signer fakeSigner) Sign(payload []byte) ([]byte, error) {
 }
 
 func TestCanSignUpWithGoogle(t *testing.T) {
-	idp := &fakeCustomProvider{}
-	ddb := &fakeDynamoDB{
-		payload: map[string]map[string]dynamodb.AttributeValue{},
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		t.Error(err)
 	}
+	cfg.Region = "ap-notheast-1"
+	cfg.EndpointResolver = aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		if service == "dynamodb" {
+			return aws.Endpoint{
+				URL:           "http://localhost:8000",
+				SigningRegion: cfg.Region,
+			}, nil
+		}
+
+		panic(fmt.Errorf(service, region))
+	})
+
+	ddb := dynamodb.New(cfg)
+	idp := &fakeCustomProvider{}
 	signer := &fakeSigner{}
 
 	testUser := struct {
@@ -104,194 +126,104 @@ func TestCanSignUpWithGoogle(t *testing.T) {
 		t.Error("Error", err)
 	}
 
-	callStackIndex := 0
-
-	// user create
-	if ddb.callStack[callStackIndex].request != "PutItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	user := ParseUser(ddb.callStack[callStackIndex].argument.(*dynamodb.PutItemInput).Item)
-	if !(user.ID == "user##fake-idp" &&
-		user.Name == testUser.Name &&
-		user.DisplayName == testUser.DisplayName &&
-		user.Picture == testUser.Picture) {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-
-	callStackIndex = 1
-
-	// user collection check
-	if ddb.callStack[callStackIndex].request != "GetItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	getItemInput := ddb.callStack[callStackIndex].argument.(*dynamodb.GetItemInput)
-	if !(*getItemInput.Key["id"].S == "collection##"+testUser.Name) {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-
-	callStackIndex = 2
-
-	// user collection put check
-	if ddb.callStack[callStackIndex].request != "PutItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	col := collection.ParseCollection(ddb.callStack[callStackIndex].argument.(*dynamodb.PutItemInput).Item)
-	if !(col.ID == testUser.Name &&
-		col.Title == testUser.Name) {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-}
-
-func TestCanSignUpWithTwitter(t *testing.T) {
-	idp := &fakeCustomProvider{}
-	ddb := &fakeDynamoDB{
-		payload: map[string]map[string]dynamodb.AttributeValue{},
-	}
-	signer := &fakeSigner{}
-
-	input := SignUpInput{
-		Form: struct {
-			Name        string `json:"name"`
-			DisplayName string `json:"display_name"`
-			Picture     string `json:"picture"`
-		}{
-			Name:        "test",
-			DisplayName: "test",
-			Picture:     "test",
+	item, err := ddb.GetItemRequest(&dynamodb.GetItemInput{
+		TableName: aws.String(os.Getenv("EntityTable")),
+		Key: map[string]dynamodb.AttributeValue{
+			"id":   {S: aws.String("user##" + identityID)},
+			"sort": {S: aws.String("user##detail")},
 		},
-		Logins: Logins{
-			Twitter: "id_token",
-		},
-	}
-	_, identityID, err := DoSignUp(input, idp, ddb, signer)
-
+	}).Send()
 	if err != nil {
-		t.Error("Error", err)
+		t.Error("error", err)
 	}
-	if identityID != "fake-idp" {
-		t.Error("Error", err)
+
+	if !(*item.Item["sort_value"].S == testUser.Name &&
+		*item.Item["display_name"].S == testUser.DisplayName &&
+		*item.Item["picture"].S == testUser.Picture) {
+		t.Errorf("Argument does not match: %+v", item.Item)
+	}
+
+	item, err = ddb.GetItemRequest(&dynamodb.GetItemInput{
+		TableName: aws.String(os.Getenv("EntityTable")),
+		Key: map[string]dynamodb.AttributeValue{
+			"id":   {S: aws.String("collection##" + testUser.Name)},
+			"sort": {S: aws.String("collection##detail")},
+		},
+	}).Send()
+	if err != nil {
+		t.Error("error", err)
+	}
+
+	if !(*item.Item["title"].S == testUser.Name) {
+		t.Errorf("Argument does not match: %+v", item.Item)
 	}
 }
 
-func TestCanSignInWithoutUserCollection(t *testing.T) {
+func TestCanSignInWithoutUserCollectionTwice(t *testing.T) {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		t.Error(err)
+	}
+	cfg.Region = "ap-notheast-1"
+	cfg.EndpointResolver = aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		if service == "dynamodb" {
+			return aws.Endpoint{
+				URL:           "http://localhost:8000",
+				SigningRegion: cfg.Region,
+			}, nil
+		}
+
+		panic(fmt.Errorf(service, region))
+	})
+
+	ddb := dynamodb.New(cfg)
+
 	testUser := User{
-		ID:   "user-id",
+		ID:   "user##user-id",
 		Name: "user-name",
 	}
 	testUserDump, _ := DumpUser(testUser)
 
-	idp := &fakeCustomProvider{}
-	ddb := &fakeDynamoDB{
-		payload: map[string]map[string]dynamodb.AttributeValue{
-			"user##fake-idp-user##detail": testUserDump,
-		},
+	_, err = ddb.PutItemRequest(&dynamodb.PutItemInput{
+		TableName: aws.String(os.Getenv("EntityTable")),
+		Item:      testUserDump,
+	}).Send()
+	if err != nil {
+		t.Error("error", err)
+	}
+
+	idp := &fakeCustomProvider{
+		customID: "user-id",
 	}
 	signer := &fakeSigner{}
 	logins := Logins{
 		Twitter: "id_token",
 	}
 
-	_, err := DoSignIn(logins, idp, ddb, signer)
+	_, err = DoSignIn(logins, idp, ddb, signer)
 	if err != nil {
 		t.Error("Error", err)
 	}
 
-	callStackIndex := 0
-
-	// user get
-	if ddb.callStack[callStackIndex].request != "GetItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
+	item, err := ddb.GetItemRequest(&dynamodb.GetItemInput{
+		TableName: aws.String(os.Getenv("EntityTable")),
+		Key: map[string]dynamodb.AttributeValue{
+			"id":   {S: aws.String("collection##" + testUser.Name)},
+			"sort": {S: aws.String("collection##detail")},
+		},
+	}).Send()
+	if err != nil {
+		t.Error("error", err)
 	}
 
-	getItemKey := ddb.callStack[callStackIndex].argument.(*dynamodb.GetItemInput).Key
-	if !(*getItemKey["id"].S == "user##fake-idp") {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-
-	callStackIndex = 1
-
-	// user collection check
-	if ddb.callStack[callStackIndex].request != "GetItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	getItemKey = ddb.callStack[callStackIndex].argument.(*dynamodb.GetItemInput).Key
-	if !(*getItemKey["id"].S == "collection##"+testUser.Name) {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-
-	callStackIndex = 2
-
-	// put user collection
-	if ddb.callStack[callStackIndex].request != "PutItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	col := collection.ParseCollection(ddb.callStack[callStackIndex].argument.(*dynamodb.PutItemInput).Item)
+	col := collection.ParseCollection(item.Item)
 	if !(col.ID == testUser.Name &&
 		col.Title == testUser.Name) {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-}
-
-func TestCanSignInWithUserCollection(t *testing.T) {
-	testUser := User{
-		ID:   "user-id",
-		Name: "user-name",
-	}
-	testUserDump, _ := DumpUser(testUser)
-
-	testUserCollection := collection.Collection{
-		ID: testUser.Name,
-	}
-	testUserCollectionDump, _ := collection.DumpCollection(testUserCollection)
-
-	idp := &fakeCustomProvider{}
-	ddb := &fakeDynamoDB{
-		payload: map[string]map[string]dynamodb.AttributeValue{
-			"user##fake-idp-user##detail":                          testUserDump,
-			"collection##" + testUser.Name + "-collection##detail": testUserCollectionDump,
-		},
-	}
-	signer := &fakeSigner{}
-	logins := Logins{
-		Twitter: "id_token",
+		t.Errorf("Argument does not match: %+v", item.Item)
 	}
 
-	_, err := DoSignIn(logins, idp, ddb, signer)
+	_, err = DoSignIn(logins, idp, ddb, signer)
 	if err != nil {
 		t.Error("Error", err)
-	}
-
-	callStackIndex := 0
-
-	// user get
-	if ddb.callStack[callStackIndex].request != "GetItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	getItemKey := ddb.callStack[callStackIndex].argument.(*dynamodb.GetItemInput).Key
-	if !(*getItemKey["id"].S == "user##fake-idp") {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-
-	callStackIndex = 1
-
-	// user collection check
-	if ddb.callStack[callStackIndex].request != "GetItemRequest" {
-		t.Error("Invalid callStack order: ", ddb.callStack[callStackIndex])
-	}
-
-	getItemKey = ddb.callStack[callStackIndex].argument.(*dynamodb.GetItemInput).Key
-	if !(*getItemKey["id"].S == "collection##"+testUser.Name) {
-		t.Errorf("Argument does not match: %+v", ddb.callStack[callStackIndex].argument)
-	}
-
-	// get user collection and done
-	if len(ddb.callStack) != 2 {
-		t.Error("Invalid callStack size: ", len(ddb.callStack))
 	}
 }
