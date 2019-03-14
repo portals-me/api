ab9b056dd9c216c58f3de346da34f0d32632085e
myuon <ioi.joi.koi.loi@gmail.com>
2019-03-15 00:13:14 +0900

    chore: reflect prod env

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -10,33 +10,30 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-module "iam" {
-  source = "../modules/iam"
-  stage = "${var.stage}"
-  service = "${var.service}"
-}
+module "portals-me" {
+  source = "../modules/portals-me"
 
-module "apigateway" {
-  source = "../modules/apigateway"
-
-  aws_region = "${var.aws_region}"
-  service = "${var.service}"
   stage = "${var.apex_environment}"
-  authorizer_arn = "${var.apex_function_authorizer}"
-  hello_arn = "${var.apex_function_hello}"
-  user_arn = "${var.apex_function_user}"
-  collection_arn = "${var.apex_function_collection}"
-  article_arn = "${var.apex_function_article}"
-  authenticator_arn = "${var.apex_function_authenticator}"
-  timeline_arn = "${var.apex_function_timeline}"
+  apex_function_hello = "${var.apex_function_hello}"
+  apex_function_user = "${var.apex_function_user}"
+  apex_function_collection = "${var.apex_function_collection}"
+  apex_function_article = "${var.apex_function_article}"
+  apex_function_authenticator = "${var.apex_function_authenticator}"
+  apex_function_authorizer = "${var.apex_function_authorizer}"
+  apex_function_entity-stream = "${var.apex_function_entity-stream}"
+  apex_function_stream-activity-feed = "${var.apex_function_stream-activity-feed}"
+  apex_function_stream-timeline-feed = "${var.apex_function_stream-timeline-feed}"
+  apex_function_timeline = "${var.apex_function_timeline}"
 }
 
-module "dynamodb" {
-  source = "../modules/dynamodb"
-  stage = "${var.stage}"
-  service = "${var.service}"
-
-  entity-stream_arn = "${var.apex_function_entity-stream}"
-  stream-activity-feed_arn = "${var.apex_function_stream-activity-feed}"
-  stream-timeline-feed_arn = "${var.apex_function_stream-timeline-feed}"
-}
+variable "apex_environment" {}
+variable "apex_function_hello" {}
+variable "apex_function_authorizer" {}
+variable "apex_function_user" {}
+variable "apex_function_collection" {}
+variable "apex_function_article" {}
+variable "apex_function_authenticator" {}
+variable "apex_function_entity-stream" {}
+variable "apex_function_stream-activity-feed" {}
+variable "apex_function_stream-timeline-feed" {}
+variable "apex_function_timeline" {}
