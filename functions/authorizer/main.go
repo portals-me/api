3814d4206fc3c068b4972faf0645294a75b07054
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-15 14:45:13 +0900

    feat: verify jwt

--- functions/authorizer/main.go
+++ functions/authorizer/main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	. "github.com/portals-me/account/functions/authenticate/lib"
 )
 
-func handler(ctx context.Context, event map[string]interface{}) (string, error) {
-	fmt.Printf("event: %+v\n", event)
-	fmt.Printf("arguments: %+v\n", event["arguments"])
-	fmt.Printf("header: %+v\n", event["request"].(map[string]interface{})["headers"])
-	return "", errors.New("Constant error message")
+var jwtPrivateKey = os.Getenv("jwtPrivate")
+
+func handler(ctx context.Context, event map[string]interface{}) ([]byte, error) {
+	fmt.Printf("%+v\n", event)
+	// Why authorization lower-case?
+	token, ok := event["request"].(map[string]interface{})["headers"].(map[string]interface{})["authorization"].(string)
+	if ok != true {
+		return nil, errors.New("Invalid Authorization Token")
+	}
+	raw := strings.TrimPrefix(token, "Bearer ")
+
+	signer := ES256Signer{
+		Key: jwtPrivateKey,
+	}
+	verified, err := signer.Verify([]byte(raw))
+	if err != nil {
+		return nil, err
+	}
+
+	var payload JwtPayload
+	if err := json.Unmarshal(verified, &payload); err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%+v\n", payload)
+	return verified, err
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/myuon/portals-me
 
 go 1.12
 
-require github.com/aws/aws-lambda-go v1.11.1
+require (
+	github.com/aws/aws-lambda-go v1.11.1
+	github.com/portals-me/account v0.0.0-20190615045028-fc4ad2c54022
+)
