8aec0304f914a01cb7a65ea950b27b1c59968117
myuon <ioi.joi.koi.loi@gmail.com>
2019-06-30 23:16:13 +0900

    fix: embed User into PostSummary

--- functions/replace-owner/main.go
+++ functions/replace-owner/main.go
@@ -9,11 +9,10 @@ import (
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/guregu/dynamo"
-	"github.com/portals-me/account/lib/user"
 )
 
 var accountTable = os.Getenv("accountTableName")
-var ownerCache = make(map[string]user.UserInfo)
+var ownerCache = make(map[string]map[string]interface{})
 
 func handler(ctx context.Context, event []map[string]interface{}) ([]map[string]interface{}, error) {
 	sess := session.Must(session.NewSession())
@@ -22,7 +21,7 @@ func handler(ctx context.Context, event []map[string]interface{}) ([]map[string]
 	authTable := db.Table(accountTable)
 
 	for _, item := range event {
-		var record user.UserInfo
+		var record map[string]interface{}
 
 		if owner, ok := ownerCache[item["owner"].(string)]; ok {
 			record = owner
@@ -37,8 +36,7 @@ func handler(ctx context.Context, event []map[string]interface{}) ([]map[string]
 			ownerCache[item["owner"].(string)] = record
 		}
 
-		item["owner_name"] = record.Name
-		item["owner_display_name"] = record.DisplayName
+		item["owner_user"] = record
 	}
 
 	return event, nil
